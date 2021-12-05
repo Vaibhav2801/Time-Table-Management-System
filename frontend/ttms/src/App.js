7c7707d98d092c114ea5d24ac1bdc200e68021d8
Abhijeet6387 <abhijeet.s.mishra@gmail.com>
2021-12-05 11:02:03 +0530

    classes component added

--- frontend/ttms/src/App.js
+++ frontend/ttms/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-// import Login from "./components/Login";
+import Classes from "./components/Classes";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
+// import Login from "./components/Login";
+// import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
       {/* <Login></Login> */}
 
       <Navbar></Navbar>
-      <Home></Home>
+      {/* <Home></Home> */}
+      <Classes></Classes>
     </div>
   );
 }
---
+++ frontend/ttms/src/components/Classes.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Classes() {
+  return (
+    <>
+      <div className="card container">
+        <div className="card-body">
+          <h5>List of Classes</h5>
+        </div>
+      </div>
+    </>
+  );
+}
