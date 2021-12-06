0a3be8cc3173136994d5a1f6f652728a3a141f67
Abhijeet6387 <abhijeet.s.mishra@gmail.com>
2021-12-06 20:43:23 +0530

    classes component update

--- frontend/ttms/src/App.js
+++ frontend/ttms/src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import Classes from "./components/Classes";
 import Navbar from "./components/Navbar";
 // import Login from "./components/Login";
-import Home from "./components/Home";
+// import Home from "./components/Home";
 // import Register from "./components/Register";
 function App() {
   return (
     <div className="App">
       {/* <Login></Login> */}
       <Navbar></Navbar>
-      <Home></Home>
-      {/* <Classes></Classes> */}
+      {/* <Home></Home> */}
+      <Classes></Classes>
       {/* <Register></Register> */}
     </div>
   );
--- frontend/ttms/src/components/Classes.js
+++ frontend/ttms/src/components/Classes.js
@@ -6,6 +6,39 @@ export default function Classes() {
       <div className="card container">
         <div className="card-body">
           <h5>List of Classes</h5>
+          <div className="classlist container-fluid">
+            <table className="table table-striped">
+              <thead className="thead-dark">
+                <tr>
+                  <th scope="col">Day</th>
+                  <th scope="col">Date</th>
+                  <th scope="col">Branch</th>
+                  <th scope="col">Subject's Name</th>
+                  <th scope="col">Subject's Code</th>
+                  <th scope="col">Lecturer</th>
+                  <th scope="col">Starts at</th>
+                  <th scope="col">Ends at</th>
+                  {/* only for teacher */}
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">Monday</th>
+                  <td>07/12/2021</td>
+                  <td>Information Technology</td>
+                  <td>DBMS</td>
+                  <td>KCS-501</td>
+                  <td>Mrs. Aditi Sharma</td>
+                  <td>10:00 AM</td>
+                  <td>10:50 AM</td>
+                  <td>
+                    <button className="btn btn-danger">Cancel</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
