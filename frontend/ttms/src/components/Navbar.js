e6b56a3cee523f5f1bedccbc88d432b62e17025a
Abhijeet6387 <abhijeet.s.mishra@gmail.com>
2021-12-08 23:45:31 +0530

    profile modal update + proxy setup

--- frontend/ttms/src/components/Navbar.js
+++ frontend/ttms/src/components/Navbar.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+// import { Link } from "react-router-dom";
 export default function Navbar() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -31,14 +37,57 @@ export default function Navbar() {
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/profile">
+                <div
+                  className="nav-link"
+                  style={{ cursor: "pointer" }}
+                  onClick={handleShow}
+                >
                   <i className="fa fa-fw fa-user"></i>Profile
-                </a>
+                </div>
               </li>
             </ul>
           </div>
         </div>
       </nav>
+      {/* profile modal */}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Profile</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="profile">
+            <div className="center">
+              <div className="row">
+                <p>
+                  <b>Name : </b>Abhijeet Mishra
+                </p>
+              </div>
+              <div className="row">
+                <p>
+                  <b>Email : </b>abc@abc.com
+                </p>
+              </div>
+              <div className="row">
+                <p>
+                  <b>Contact : </b>+91 8833456783
+                </p>
+              </div>
+              <div className="row">
+                <p>
+                  <b>Role : </b>Student
+                </p>
+              </div>
+            </div>
+            <button
+              type="button"
+              className="btn btn-danger"
+              onClick={handleClose}
+            >
+              Logout
+            </button>
+          </div>
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
--- server.js
+++ server.js
@@ -1,32 +1,25 @@
-const express=require('express')
-const bodyParser=require('body-parser')
-const student=require('./routes/student')
-const teacher=require('./routes/teacher')
-const schedule=require('./routes/schedule')
-const db =require('./db.js')
-const passport=require('passport')
+const express = require("express");
+const bodyParser = require("body-parser");
+const student = require("./routes/student");
+const teacher = require("./routes/teacher");
+const schedule = require("./routes/schedule");
+const db = require("./db.js");
+const passport = require("passport");
 
-
-
-const app=express()
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json()) 
-
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //sql connection
-db.connection
+db.connection;
 
+app.use("/student", student);
+app.use("/teacher", teacher);
+app.use("/teacher/schedule", schedule);
 
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome" });
+});
 
-app.use('/student',student)
-app.use('/teacher',teacher)
-app.use('/teacher/schedule',schedule)
-
-
-app.get('/',(req,res)=>{
-    res.json({message:"Welcome"})
-})
-
-
-const port=process.env.PORT || 3000
-app.listen(port,()=> console.log('Server is up on port '+port))
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log("Server is up on port " + port));
